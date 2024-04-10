3c36c662a182edd22d7af324a7776079dd92030a
AndrewJacop <andrewjacop@ymail.com>
2024-04-10 21:44:41 +0200

    fixed Adbar small adjustments

--- src/components/ads/AdBar.tsx
+++ src/components/ads/AdBar.tsx
@@ -1,35 +1,31 @@
 import Image from "next/image";
 /**
  * @breif Ad Bar
- * this component is Ad bar for credits with offer description and credit img 
+ * this component is Ad bar for credits with offer description and credit img
  */
 export default function AdBar() {
   return (
-    <main className="flex  flex-col p-0 px-6 mb-6">
-      <div className="text-center justify-center  flex items-center bg-blue-50 rounded-lg py-2 w-full px-20 ">
-        <Image
-          src="https://i5.walmartimages.com/dfw/4ff9c6c9-5626/k2-_9f1ca88f-5819-4c4c-8ae1-e15a46420d9b.v1.png?odnHeight=64&odnWidth=107&odnBg=FFFFFF"
-          alt="Your Image"
-          width={80}
-          height={80}
-        />
-        <div className=" px-3">
-          <h1 className="text-[12px]  font-bold text-[#172026] inline">
-            Earn 5% cash back on Walmart.com.
-          </h1>
-          <p className="text-[11px] text-[#36485C] lg:leading-7 inline">
-            See if youre pre-approved with no credit risk.
-          </p>
-          <p className=" px-4 text-[10px]  text-[#36485C] lg:leading-7 inline">
-            <a
-              href="https://example.com"
-              className="text-[#36485C] text-sm px-4 hover:border-[2px] border-[1px] border-[black] bg-[white] rounded-[20px] py-2"
-            >
-              Learn how.
-            </a>
-          </p>
-        </div>
-      </div>
-    </main>
+    <div className="justify-center flex items-center bg-blue-50 rounded-lg py-2 my-2 w-full shadow-sm shadow-slate-400">
+      <Image
+        src="https://i5.walmartimages.com/dfw/4ff9c6c9-5626/k2-_9f1ca88f-5819-4c4c-8ae1-e15a46420d9b.v1.png?odnBg=FFFFFF"
+        alt="Ad bar image"
+        width={81}
+        height={51}
+        className="mx-4"
+      />
+      <p className="px-1 text-base font-bold">
+        Earn 5% cash back on Walmart.com.
+      </p>
+      <p className="px-1 text-base text-gray-700 lg:leading-7">
+        See if youre pre-approved with no credit risk.
+      </p>
+      <p className="px-2 text-sm text-gray-700 lg:leading-7">
+        <a
+          href="/"
+          className="text-gray-700 text-sm px-4 border-2 border-black bg-white rounded-full py-2">
+          Learn More
+        </a>
+      </p>
+    </div>
   );
 }
