cdf1e11051b2597a9d1c196f3a5f2282534c33e9
AndrewJacop <andrewjacop@ymail.com>
2024-04-21 20:38:44 +0200

    adjusted navigation to cart

--- src/components/navbar/NewTopBar.tsx
+++ src/components/navbar/NewTopBar.tsx
@@ -1,9 +1,6 @@
-import Image from "next/image";
 import { LayoutGrid } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
-
-import WalmartLogo from "../../../public/walmartLogo.svg";
 import SearchBar from "./SearchBar";
 import CartToggle from "./CartToggle";
 import { getCurrentUser } from "@/lib/firebase/admin-config";
@@ -11,7 +8,7 @@ import { getUserByUid } from "@/lib/supabase/fetch-data";
 import { signMeOut } from "@/lib/firebase/auth";
 import MyItemsButton from "./MyItemsButton";
 import AccountButton from "./AccountButton";
-import Link from "next/link";
+import MainLogo from "./MainLogo";
 
 export const revalidate = 0;
 export const dynamic = "force-dynamic";
@@ -36,13 +33,7 @@ export default async function NewTopNavBar() {
   return (
     <div className="flex justify-center items-center px-4 py-6 bg-[#0071dc] gap-1 z-10">
       {/* Logo */}
-      <Link href='/'>
-      <Button
-        variant="ghost"
-        className="text-white font-bold rounded-full py-4">
-        <Image priority src={WalmartLogo} alt="Logo" />
-      </Button>
-      </Link>
+      <MainLogo />
       {/* Departments Button */}
       <Button variant="ghost" className="text-white font-bold rounded-full">
         <LayoutGrid className="mr-2 h-4 w-4" />
